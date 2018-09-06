3786d5c3df41d5c8c9ff8b58746009ec40b120c6
Glen <Cplusplusmin@gmail.com>
2018-09-05 14:16:17 -1000

    Added main.cpp and Makefile for Homework #2

---
+++ hw02/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include "powerup.h"
+
+int main(int argc, char** argv) {
+	
+	//prints the result of the function which would raise the first number to the power of the second number
+	printf("%d\n", powerup(4, 5));
+}
