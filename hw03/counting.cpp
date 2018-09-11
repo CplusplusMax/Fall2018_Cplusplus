0b720149a28450740edd2614a5abd9f0febafc9a
Glen <Cplusplusmin@gmail.com>
2018-09-10 15:44:03 -1000

    Uploading first crack at HW03 code

---
+++ hw03/counting.cpp
@@ -0,0 +1,11 @@
+#include "counting.h"
+
+int counting() {
+	// declares the incremental variable "bam" and initializes the static variable to 0
+	int bam = 0;
+	
+	++bam;
+	
+	return bam;
+	
+}
---
+++ hw03/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include "counting.h"
+
+int main(int argc, char** argv) {
+
+	int bam = counting();
+	printf("The number of times this program has run is %d.\n", bam);
+
+}
