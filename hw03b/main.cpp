d1bdd1ae059896848b59b60de9ffdebb817748fb
Glen <Cplusplusmin@gmail.com>
2018-09-15 03:00:27 -1000

    Ahhhh, reuploaded program #2 from hw03b directory, I found that I needed to open up a separate command prompt window, cd to the directory and then run the executable file from there, and all prints on screen...yay :)

--- hw03b/main.cpp
+++ hw03b/main.cpp
@@ -6,17 +6,14 @@ int main(int argc, char** argv) {
 	int one, two;
 	
 	printf("Think about two positive integers between 1 and 100.\n");
-	printf("Enter the first integer: ");
-	scanf("%d\n", &one);
-	
-	printf("Enter the second integer: ");
-	scanf("%d\n", &two);
+	printf("Enter the two integers separated by a space: ");
+	scanf("%d %d", &one, &two);
 	
 	printf("The two integers you entered were: %d, %d.\n", one, two);
 
 	swaahp(&one, &two);
 	
-	printf("The two numbers in numerical order are: %d, %d/n", one, two);
+	printf("The two numbers in numerical order are: %d, %d\n", two, one);
 	
 	return 0;
 
