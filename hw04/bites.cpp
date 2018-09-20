676193104ae1395414730793e9890ff1e0fc50bc
Glen <Cplusplusmin@gmail.com>
2018-09-19 14:26:46 -1000

    initial Working parts of homework #04

---
+++ hw04/bites.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include "bites"
+
+int main(int argc, char** argv) {
+
+	// open a file that has binary data
+	FILE* fp = fopen("test.ppm", "r");
+	
+	int array[128];
+	
+	for(int=0; string[i] != '\0' && i<size; ++i)
+	
+		{
+		fread(array, sizeof(int), 128, fp);
+		
+		}
+		//return the pixel data in a one dimensional array of unsigned chars
+		// plus the width and heighth of the image.
+		fclose(fp);
+	
+	
+	
+	
---
+++ hw04/bites.h
@@ -0,0 +1,6 @@
+#ifndef BITES_H
+#define SQUARED_H
+
+int snoopie(int x);
+
+#endif
