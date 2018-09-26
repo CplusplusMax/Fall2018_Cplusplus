d08c89d5c1fb02dcc4e7588ad92e574b167ea10e
Glen <Cplusplusmin@gmail.com>
2018-09-26 11:25:26 -1000

    This umpteen.cpp file successfully prints the magic number, the width and height and the max of 255

---
+++ hw04a/umpteen.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include "umpteen.h"
+
+
+int main(int argv, char** argc) {
+	//open the file
+	
+	FILE* fp = fopen("bitetest.ppm", "rb");
+	
+	// Declare variables and initialize array for magic number to 3 "P +N +\0"
+	char magic[3];
+	int width, height, max;
+	int numBytes = width*height*3;
+	
+	// read Magic Number which is a string
+	fscanf(fp, "%s", magic);
+	printf("%s\n", magic);
+	
+	// read width and height which are integers
+	fscanf(fp, "%d %d\n", &width, &height);
+	printf("%d %d\n", width, height);
+		
+	// read Max bytes which is set to the integer value of 255
+	fscanf(fp, "%d", &max);
+	printf("%d", max);
+	
+	// read binary data
+	//unsigned char* bytes = new unsigned char[numBytes];
+	
+	//fread(bytes, sizeof(bytes), numBytes, fp);
+	
+	
+}
+	
